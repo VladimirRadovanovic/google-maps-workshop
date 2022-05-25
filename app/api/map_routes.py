cc73bcc7bd1ffed912a5f048713bc4f224d8f72e
Vladimir Radovanovic <vladimirradovanovic1990@gmail.com>
2022-05-25 16:25:21 -0400

    query by distance

--- app/api/map_routes.py
+++ app/api/map_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import Listing
 import os
+from sqlalchemy import text
 
 map_routes = Blueprint('map', __name__)
 
@@ -13,17 +14,28 @@ def load_map_key():
     return {'googleMapsAPIKey': key}
 
 
-@map_routes.route('/<string:address>')
+@map_routes.route('/<string:address>/<string:lat>/<string:lng>')
 @login_required
-def get_places(address):
-    print(address, '/n/n/n/n')
+def get_places(address, lat, lng):
+    print(address, lat, lng, '/n/n!!!!!!!!!!!!!!!!!!!/n/n')
 
     parsed_city = address[:-9]
+    distance = 20
 
     if ',' in parsed_city:
         i = parsed_city.index(',')
         parsed_city = parsed_city[i+2:]
 
-    places = Listing.query.filter(Listing.city == parsed_city).all()
+    # places = Listing.query.filter(Listing.city == parsed_city).all()
+
+    clause = "SQRT(POW(69.1 * (lat - :lati),2) + POW(53.0 * (lng - :long),2)) < :dis"
+# query = meta.Session.query(User).filter(clause).params(lat=my_latitude, long=my_longitude)
+
+    places = Listing.query.filter(text(clause)).params(lati=lat, long=lng, dis=distance).all()
+
+    # places = Listing.execute()
+
+    print(places, '\n!!!!!!!!!!!!!!!!!!!!\n\n')
+
 
     return {'places': [place.to_dict() for place in places]}
